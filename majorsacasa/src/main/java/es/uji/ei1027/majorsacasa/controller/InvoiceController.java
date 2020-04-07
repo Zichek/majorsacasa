12317449e95fe559169e86f0d97fb45e7ff9ddc1
Eric <Eric@DESKTOP-M53CMNS>
2020-04-07 19:44:18 +0200

    Invoice invoiceDao invoiceRowMapper invoiceController add Invoice update Invoice list Invoice

---
+++ majorsacasa/src/main/java/es/uji/ei1027/majorsacasa/controller/InvoiceController.java
@@ -0,0 +1,73 @@
+package es.uji.ei1027.majorsacasa.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import es.uji.ei1027.majorsacasa.dao.CompanyDao;
+import es.uji.ei1027.majorsacasa.dao.InvoiceDao;
+import es.uji.ei1027.majorsacasa.model.Company;
+import es.uji.ei1027.majorsacasa.model.Invoice;
+
+
+@Controller
+@RequestMapping("/invoice") 
+public class InvoiceController {
+
+   private InvoiceDao invoiceDao;
+
+   @Autowired
+   public void setInvoiceDao(InvoiceDao invoiceDao) { 
+       this.invoiceDao=invoiceDao;
+   }
+
+   // Operacions: Crear, llistar, actualitzar, esborrar
+   
+   @RequestMapping("/list")
+   public String listCompanys(Model model) {
+      model.addAttribute("invoices", invoiceDao.getAllInvoice());
+      return "invoice/list";
+   }
+   
+   @RequestMapping(value="/add") 
+	public String addInvoice(Model model) {
+		model.addAttribute("invoice", new Invoice());
+		return "invoice/add";
+	}
+   @RequestMapping(value="/add", method=RequestMethod.POST)
+   public String processAddSubmit(@ModelAttribute("invoice") Invoice invoice,
+                                   BindingResult bindingResult) { 
+   	 if (bindingResult.hasErrors()) 
+   			return "invoice/add";
+   	 invoiceDao.addInvoice(invoice);
+   	 return "redirect:list"; 
+    }
+
+
+   @RequestMapping(value="/update/{number}", method = RequestMethod.GET) 
+	public String editInvoice(Model model, @PathVariable int number) { 
+		model.addAttribute("invoice", invoiceDao.getInvoice(number));
+		return "invoice/update"; 
+	}
+   @RequestMapping(value="/update", method = RequestMethod.POST) 
+	public String processUpdateSubmit(
+                           @ModelAttribute("invoice") Invoice invoice, 
+                           BindingResult bindingResult) {
+		 if (bindingResult.hasErrors()) 
+			 return "invoice/update";
+		 invoiceDao.updateInvoice(invoice);;
+		 return "redirect:list"; 
+	}
+
+   @RequestMapping(value="/delete/{number}")
+	public String processDelete(@PathVariable int number) {
+          invoiceDao.deleteInvoice(number);
+          return "redirect:../list"; 
+	}
+
+}
---
+++ majorsacasa/src/main/java/es/uji/ei1027/majorsacasa/dao/InvoiceRowMapper.java
@@ -0,0 +1,21 @@
+package es.uji.ei1027.majorsacasa.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+import org.springframework.jdbc.core.RowMapper;
+import es.uji.ei1027.majorsacasa.model.Invoice;
+
+public final class InvoiceRowMapper implements RowMapper<Invoice> {
+	public Invoice mapRow(ResultSet rs, int rowNum) throws SQLException {
+		Invoice invoice = new Invoice();
+		invoice.setNumber(rs.getInt("number"));
+		invoice.setElderlyDNI(rs.getString("elderlyDNI"));
+		invoice.setRequestNumber(rs.getInt("requestNumber"));
+		invoice.setDate(rs.getObject("date", LocalDate.class));
+		invoice.setAmount(rs.getInt("amount"));
+		invoice.setConcept(rs.getString("concept"));
+		return invoice;
+	}
+}
