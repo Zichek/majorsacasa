c2b9f7ecbc6fd2d5a784715a41a62e3717194d0f
Rosu <Rosu@DESKTOP-DT4HNOP>
2020-05-04 16:44:54 +0200

    Login bonito

--- majorsacasa/src/main/java/es/uji/ei1027/majorsacasa/MajorsacasaApplication.java
+++ majorsacasa/src/main/java/es/uji/ei1027/majorsacasa/MajorsacasaApplication.java
@@ -5,6 +5,7 @@ import java.util.logging.Logger;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
+
 @SpringBootApplication
 public class MajorsacasaApplication {
 
